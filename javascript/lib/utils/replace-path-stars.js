60f3a52dd69037ba676671077012aa3b1cfd203d
Tamay Eser Uysal <tamayeseruysal@gmail.com>
2020-04-29 19:26:52 +0000

    Made replace path stas's regex better

--- javascript/lib/utils/replace-path-stars.js
+++ javascript/lib/utils/replace-path-stars.js
@@ -7,26 +7,26 @@ function replacePathStars(paths) {
             newPaths.push(path)
             continue
         }
-
+        
         const prefix = path.replace(/\[?\*.*/, "")
-
+        
         const isNewItem = path.replace(prefix, "").replace(/\*\]?/, "").length === 0
-
+        
         let lastNumber = null
         for (let j = newPaths.length - 1; j >= 0; j--) {
             if (newPaths[j].startsWith(prefix)) {
                 lastNumber = parseInt(
-                    newPaths[j].replace(prefix, "").match(/\[?([0-9])+/)[1]
+                    newPaths[j].replace(prefix, "").match(/\[?([0-9]+)/)[1]
                 )
                 break
             }
         }
-
+        
         const newPath = path.replace(
             "*",
             lastNumber === null ? 0 : isNewItem ? lastNumber + 1 : lastNumber
         )
-
+        
         newPaths.push(newPath)
     }
 
