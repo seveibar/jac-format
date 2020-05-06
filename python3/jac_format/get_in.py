8003397a41d23ee26d850462da93e3836e42945b
seveibar <seveibar@gmail.com>
2020-05-06 09:48:56 -0400

    get in implementation

--- python3/jac_format/get_in.py
+++ python3/jac_format/get_in.py
@@ -1,2 +1,28 @@
-def get_in():
-    pass
+import re
+
+
+def get_in_array_path(d, path):
+    print(d, path)
+    if d is None:
+        return None
+    if len(path) == 0:
+        return d
+    if isinstance(d, list):
+        if not isinstance(path[0], int):
+            return None
+        if path[0] >= len(d):
+            return None
+        return get_in_array_path(d[path[0]], path[1:])
+    else:
+        return get_in_array_path(d.get(path[0], None), path[1:])
+
+
+def get_in(d, path):
+    path = re.sub(r"\[([0-9]+)\]", r".\1", path)
+    path = [int(seg) if seg.isnumeric() else seg for seg in path]
+    return get_in_array_path(d, path)
+
+
+if __name__ == "__main__":
+    assert get_in([{"b": [None, "c"]}], "0.b.1") == "c"
+    assert a[0]["b"][1] == "c"
