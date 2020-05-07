8a7f3898583152e67fdcff8d131a42b1f603cc8a
seveibar <seveibar@gmail.com>
2020-05-07 00:29:25 -0400

    finish setup.py, add license

--- python3/setup.py
+++ python3/setup.py
@@ -1,20 +1,28 @@
+from path import join
 from setuptools import setup, find_packages
 
 
-with open('README.md') as f:
+with open(join(__dirname, "../README.md")) as f:
     readme = f.read()
 
-with open('../LICENSE') as f:
+with open(join(__dirname, "../LICENSE")) as f:
     license = f.read()
 
 setup(
-    name='jac_format',
-    version='0.1.0',
-    description='JAC (JSON as CSV) Format Conversion',
+    name="jac_format",
+    version="0.1.0",
+    description="JAC (JSON as CSV) Format Conversion",
     long_description=readme,
-    author='UniversalDataTool',
-    author_email='info@workaround.online',
-    url='https://github.com/UniversalDataTool/jac-format',
+    long_description_content_type="text/markdown",
+    author="UniversalDataTool",
+    author_email="seve@wao.ai",
+    url="https://github.com/UniversalDataTool/jac-format",
     license=license,
-    packages=find_packages(exclude=('tests', 'docs'))
+    packages=["jac_format"],
+    classifiers=[
+        "Development Status :: 2 - Pre-Alpha",
+        "Intended Audience :: Science/Research",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3.6",
+    ],
 )
